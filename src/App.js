3980587e866cb476973541da69247c2f415e940f
Despat Tanguy <tanguydespat@gmail.com>
2022-02-07 11:25:36 +0100

    anecdotes step 1

--- src/App.js
+++ src/App.js
@@ -48,6 +48,43 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 }
 
+const Anecdotes = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  // random int between 0 and 6
+  const randomNumber = Math.floor(Math.random() * 7)
+  const [selectedAnecdote, setSelectedAnecdote] = useState(randomNumber)
+  // console.log(selectedAnecdote)
+
+  const handleClick = () => {
+    const lastAnecdote = selectedAnecdote
+    const randomNumber = Math.floor(Math.random() * 7)
+    if(randomNumber === lastAnecdote) {
+      // générer nouveau numéro
+      const randomNumber = Math.floor(Math.random() * 7)
+      setSelectedAnecdote(randomNumber)
+    }
+    else {
+      setSelectedAnecdote(randomNumber)
+    }
+  }
+
+  return (
+    <div>
+      <h2>anecdotes</h2>
+      <Button text='random anecdote' handleClick={handleClick} />
+      <p>{anecdotes[selectedAnecdote]}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -64,6 +101,8 @@ const App = () => {
         <Button text='bad' handleClick={() => setBad(bad + 1)} />
 
         <Statistics good={good} neutral={neutral} bad={bad} />
+
+        <Anecdotes />
       </div>
     </div>
   )
